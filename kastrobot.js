c6ac8b5676922a58cbfe7abb313b06b6e8aab33d
NekoVikA <drobovike@gmail.com>
2018-01-13 13:13:18 +0200

    kaasto on login

--- kastrobot.js
+++ kastrobot.js
@@ -1,5 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const MH_GUILD_ID = '270727104319062026';
+
+const user_list={"Lier": "270723318427025418",//Lier
+                 "Vika": "270911343274491905",//Vika
+                 "Mudj": "271006635126816778",//Mudj
+                 "Kaastro": "270974444871221259"//Volera
+}
 
 var kastrords = [/k(a*)tso/g,/k(a*)stro/g,/к(а*)стро/g,/к(а*)тсо/g];
 
@@ -12,7 +19,10 @@ var madj_2 = ["адж","нядж","идж","ядж","удж"]
 
 client.on("ready", () => {
     client.user.setUsername("CHOPK");
-    //console.log(client.guilds);
+    MH_GUILD = client.guilds.get(MH_GUILD_ID);
+    GUILD_CH = MH_GUILD.channels.find(ch => ch.name === 'guild');
+    G_RANG_CH = MH_GUILD.channels.find(ch => ch.name === 'g-rank');
+
     // This event will run if the bot starts, and logs in, successfully.
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
     // Example of changing the bot's playing game to something useful. `client.user` is what the
@@ -23,16 +33,13 @@ client.on("ready", () => {
 
 //todo on kaaastro login greet kaaastro
 client.on("presenceUpdate", (old, new_) => {
-    user_list=["270723318427025418",//Lier
-               "270911343274491905",//Vika
-               "271006635126816778",//Mudj
-               "270974444871221259"//Volera
-    ]
-    if (user_list.indexOf(new_.user.id)>=0){
+    if (Object.values(user_list).indexOf(new_.user.id)>=0){
         //console.log(old.presence.status, new_.presence.status, ['idle', 'online', 'dnd'].indexOf(new_.presence.status));
         if (old.presence.status == 'offline' && ['idle', 'online', 'dnd'].indexOf(new_.presence.status) >= 0){
-            var ch_ = new_.guild.channels.find(ch => ch.name === 'g-rank');
-            ch_.send(new_.user.username+' logged in!');
+            G_RANG_CH.send(new_.user.username+' logged in!');
+            if (new_.user.id == user_list['Kaastro']){
+                GUILD_CH.send('Бажаємо здоров\'я, товаришу Кааастро!')
+            }
         }
     }
 });
