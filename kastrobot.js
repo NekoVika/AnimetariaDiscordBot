da818c8c0674a5cd8fa9f3e7bd3541cb32852ac0
NekoVikA <drobovike@gmail.com>
2018-01-14 23:19:31 +0200

    commands

--- kastrobot.js
+++ kastrobot.js
@@ -45,20 +45,30 @@ client.on("presenceUpdate", (old, new_) => {
 });
 client.on("message", async message => {
     if(message.author.bot) return;
-    for(i=0,x=kastrords.length;i<x;i++){
-        if(message.content.toLowerCase().search(kastrords[i])>=0){            
-            message.channel.send("Слався Кааастризм его великий! Да прибудет воля Кааастро!");
+    if (message.content.startsWith('!')){
+        switch(message.content){
+            case '!games': 
+            message.channel.send("В лол скатайте");
             break;
-        };
-    };
-
-    for(i=0,x=madjords.length;i<x;i++){
-        var found_w=message.content.match(madjords[i]);
-        if(found_w){
-            var right_madj = madj_1[Math.floor((Math.random() * madj_1.length))]+madj_2[Math.floor((Math.random() * madj_2.length))]
-            message.channel.send(">"+found_w[0]+" Возможно вы имели ввиду "+right_madj);            
         }
-    };
+        return;
+    }else{
+        for(i=0,x=kastrords.length;i<x;i++){
+            if(message.content.toLowerCase().search(kastrords[i])>=0){            
+                message.channel.send("Слався Кааастризм его великий! Да прибудет воля Кааастро!");
+                break;
+            };
+        };
+    
+        for(i=0,x=madjords.length;i<x;i++){
+            var found_w=message.content.match(madjords[i]);
+            if(found_w){
+                var right_madj = madj_1[Math.floor((Math.random() * madj_1.length))]+madj_2[Math.floor((Math.random() * madj_2.length))]
+                message.channel.send(">>"+found_w[0]+" Возможно, вы имели ввиду "+right_madj+"?");            
+            }
+        };
+    }
+    
 
     
     // const words = message.content.trim().split(/ +/g);
