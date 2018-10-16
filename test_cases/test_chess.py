6929eabc911002da3da94df6b032ee7ebd00c7fa
Oleg D <drobovike@gmail.com>
2018-10-16 17:26:04 +0300

    some updates to tests

--- test_cases/test_chess.py
+++ test_cases/test_chess.py
@@ -36,6 +36,27 @@ class TestGame(unittest.TestCase):
         self.assertFalse(g.validate_spelling('f1f2'))
 
 
+class TestPieces(unittest.TestCase):
+    field = None
+    game = None
+
+    def setUp(self):
+        self.field = main.ChessField()
+        self.game = main.ChessGame(self.field)
+
+    def tearDown(self):
+        self.field = None
+        self.game = None
+
+    def test_pawn_move_forward_freely(self):
+        field = self.game.field
+        field.init_piece('a1', self.game.player1, pieces.ChessPawn)
+        pawn = self.game.field.get_by_id('a1').piece
+        res = self.game.make_move(('a1', 'a2'))
+        self.assertEqual(res['code'], 0)
+        self.assertEqual(self.game.field.get_by_id('a2').piece, pawn)
+
+
 class TestHelperPoint(unittest.TestCase):
 
     def test_helper_point_init_empty(self):
